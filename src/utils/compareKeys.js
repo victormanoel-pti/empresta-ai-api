d2ef38b1e73c1ef433bebaf1b9a6f9041d5454af
“Victor <144040650+victormanoel-pti@users.noreply.github.com>
2023-11-09 20:56:01 -0300

    adiciona funcao para validar keys de objeto

---
+++ src/utils/compareKeys.js
@@ -0,0 +1,9 @@
+
+
+function compareKeys(objSrc, objTgt) {
+    var objSrcKeys = Object.keys(objSrc).sort();
+    var objTgtKeys = Object.keys(objTgt).sort();
+    return JSON.stringify(objSrcKeys) === JSON.stringify(objTgtKeys);
+}
+
+export default compareKeys;
