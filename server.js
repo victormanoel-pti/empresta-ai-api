0c190614744bcdc73342b6eb2efcf3df6d32e04a
“Victor <144040650+victormanoel-pti@users.noreply.github.com>
2023-11-08 23:09:46 -0300

    adiciona express

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-import http from "http"
+//import http from "http"
+
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const server = http.createServer((req, res)=>{
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("OK");
-});
-
-server.listen(PORT, ()=>{console.log(`Running server on port ${PORT}.`)});
+app.listen(PORT, ()=>{console.log(`Running server on port ${PORT}.`)});
