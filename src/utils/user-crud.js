555f2c18718ea39b060cfc75018a4c38e02a78c8
“Victor <144040650+victormanoel-pti@users.noreply.github.com>
2023-11-16 19:36:48 -0300

    adiciona "crud" de usuarios

---
+++ src/utils/user-crud.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+const filePath = `/src/utils/users.json`;
+
+const crud = {
+    updateUsers(data){
+        let fileData = JSON.parse(fs.readFileSync(`${process.cwd()}${filePath}`));
+        fileData.users.push(data);
+        fs.writeFileSync(`${process.cwd()}${filePath}`, JSON.stringify(fileData, null, 2));
+    },
+
+    findUserByEmail(email){
+        let fileData = JSON.parse(fs.readFileSync(`${process.cwd()}${filePath}`));
+        if(fileData.users.length === undefined || fileData.users.length === "undefined"){
+            return {
+                "found": false,
+                "user": fileData.users[i]
+            }
+        }
+        for (let i = 0; i < fileData.users.length; i++) {
+            if(fileData.users[i].email === email){
+                return {
+                    "found": true,
+                    "user": fileData.users[i]
+                }
+            }else{
+                return -1;
+            }
+        }
+        
+    },
+
+    findLogin(email, password){
+        let fileData = JSON.parse(fs.readFileSync(`${process.cwd()}${filePath}`));
+        const {users} = fileData;
+        for(let i = 0; i < users.length; i++){
+            if(users[i].email === email && users[i].senha === password){
+                return users[i];
+            }
+            return false;
+        }
+    }
+
+}
+
+export default crud;
