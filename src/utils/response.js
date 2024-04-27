c88e217c3a6e0ca25af10f42fa424ccca68846e0
“Victor <144040650+victormanoel-pti@users.noreply.github.com>
2024-04-27 18:00:22 -0300

    Adiciona novos packages e altera resposta conforme boolean

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,8 +1,5 @@
 function response(success, message){
-    return({
-        "success": success,
-        "message": message
-    });
+    return((success) ? {"success" : success, data: message} : {"success" : success, "message": message});
 }
 
 export default response;
