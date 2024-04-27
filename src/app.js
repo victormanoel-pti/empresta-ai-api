7825bc56a75874413909b95455a71dbfd6eb1259
“Victor <144040650+victormanoel-pti@users.noreply.github.com>
2024-04-27 19:23:46 -0300

    Adiciona validacao por jwt, implementa get/amigos, post/cadastro, post/login

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import jsonwebtoken from "jsonwebtoken";
 import { createClient } from "@supabase/supabase-js";
 import { validationResult } from "express-validator";
-import "dotenv/config.js"
+import "dotenv/config.js";
 import response from "./utils/response.js";
 import { cadastroValidator } from "./utils/validators.js";
 import { doesEmailExists } from "./utils/findUserEmail.js";
@@ -11,13 +11,9 @@ import { hash } from "bcrypt";
 
 
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
-
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 const app = express();
-const secret = "123";
-
 let jwt = jsonwebtoken;
-let blacklist = [];
 
 
 app.use(cors({
@@ -26,26 +22,15 @@ app.use(cors({
 
 app.use(express.json());
 
-function checkJwt(req, res, next) {
-    const token = req.headers["authorization"];
-    jwt.verify(token, secret, (err, decodedJwt)=> {
-        if(err || blacklist.includes(token)){
-            console.log(blacklist)
-            return res.status(401).json();
-        }
-        req.id = decodedJwt.id;
-        next();
-    })
-}
-
 function supabaseJwt(req, res, next){
     const token = req.headers["authorization"];
-    jwt.verify(token, secret, (error, decodedJwt) => {
+    jwt.verify(token, process.env.SUPABASE_JWT_SECRET, async (error, decodedJwt) => {
         if(error){
-            console.log(error)
+            return res.status(401).json(response(true, "Token inválido."));
         }else{
-            console.log(decodedJwt)
-            req.userId = decodedJwt.id;
+            const {data} = await supabase.from('usuarios').select('id').eq('email', decodedJwt.email );
+            req.userEmail = decodedJwt.email;
+            req.userId = data[0].id; // id do usuário no banco para consultas
             next();
         }
     });
@@ -56,28 +41,28 @@ app.get("/", (req, res)=> {
     res.status(200).json(response(true, "API Funcionando!"));
 });
 
-app.get("/amigos", async (req, res)=> {
-   let {error, data} = await supabase.from('usuarios').select('amigos').eq('id', 8);
-   if(error === null && data.length <= 0){
+app.get("/amigos" , supabaseJwt,async (req, res)=> {
+   let {data} = await supabase.from('usuarios').select('amigos').eq('id', req.userId);
+   if(data[0].amigos.length <= 0 ){
     return res.status(404).json(response(false, "Usuário não possui amigos cadastrados."));
    }else{
     return res.status(200).json(response(true, data));
    }
 });
 
-app.post("/amigos", checkJwt, (req, res)=> {
+app.post("/amigos", (req, res)=> {
     /**
      * TODO: Implementar a validação dos campos e efetuar a criação de amigos
      */
 });
 
-app.delete("/amigos/excluir/:id", checkJwt, (req, res)=> {
+app.delete("/amigos/excluir/:id", (req, res)=> {
     /**
      * TODO: Implementar a remoção de amigos
      */
 });
 
-app.get("/usuarios", async (req, res)=> {
+app.get("/usuarios", supabaseJwt ,async (req, res)=> {
     let { data: usuarios, error } = await supabase.from('usuarios').select('*');
     if(error){
         return res.status(400).json(response(false, "Erro ao buscar usuários."));
@@ -88,23 +73,18 @@ app.get("/usuarios", async (req, res)=> {
 });
 
 app.post("/login", async(req, res)=> {
-    
-    //let buscarEmail = await supabase.from('usuarios').select('email').eq('email', req.body.email);
-    //if(buscarEmail.data.length <= 0 || buscarEmail.error){
-    //    return res.status(401).json(response(false, "Usuário não possui cadastro."));
-    //}else{
-        //console.log(`Achou o email ${buscarEmail.data[0].email}`);
-        
-        let responseSupa = await supabase.auth.signInWithPassword({
-            email: req.body.email,
-            password: req.body.senha
-        });
-        return res.status(200).json(responseSupa);
-    //}
-    
+    let {data, error} = await supabase.auth.signInWithPassword({
+        email: req.body.email,
+        password: req.body.senha
+    });
+    if(error){
+        return res.status(401).json(response(true, "Usuário ou senha inválido(s)."));
+    }else{
+        return res.status(201).json(response(true, data.session.access_token));
+    }
 });
 
-app.post("/logout", checkJwt,(req, res)=> {
+app.post("/logout",(req, res)=> {
     /**
      * TODO: Implementar logout e desabilitar o token
      */
@@ -117,25 +97,25 @@ app.post("/cadastro", cadastroValidator, async (req, res)=> {
         if (await doesEmailExists(email)){
             return res.status(400).json(response(false, "Email já cadastrado."));
         }else{
-            const { data, error } = await supabase
-            .from('usuarios')
+            await supabase.from('usuarios')
             .insert([
                 { nome, sobrenome, cidade, genero, data_nascimento: dataNascimento, email, senha: await hash(senha, 8), amigos},
             ])
             .select()
+            await supabase.auth.signUp({email, password: senha})
             return res.status(201).json(response(true, "Usuário cadastrado com sucesso."));
         }
     }
     return res.status(400).json(errors.array());
 });
 
-app.post("/grupos/criar", checkJwt,(req, res) => {
+app.post("/grupos/criar",(req, res) => {
     /**
      * TODO: Implementar adicição de grupo
      */   
 });
 
-app.get("/grupos/meus-grupos", checkJwt, (req, res)=> {
+app.get("/grupos/meus-grupos", (req, res)=> {
 
     /** 
      * Implementar busca de grupos do usuario
@@ -156,7 +136,7 @@ app.get("/grupos/detalhes/:id", (req, res)=> {
      */
 });
 
-app.patch("/grupos/editar/:id", checkJwt, (req,res)=> {
+app.patch("/grupos/editar/:id", (req,res)=> {
     /**
      * Implementar edicao do grupo
      */
